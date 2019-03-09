24aa3b5f82b2ce42fbc2fa6d23fa313c67419ac4
Valeriy-Sorochynkyi <haribol162@gmail.com>
2019-03-09 12:25:15 +0200

    Updates

---
+++ precache-manifest.067033a71e52699fbdca48f213aa6467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cee85bdd30308b1a7b0",
+    "url": "/swapi-app/static/css/main.82143c34.chunk.css"
+  },
+  {
+    "revision": "8cee85bdd30308b1a7b0",
+    "url": "/swapi-app/static/js/main.17272631.chunk.js"
+  },
+  {
+    "revision": "29fef7c567a4a9ee30b2",
+    "url": "/swapi-app/static/js/runtime~main.fb28db7b.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/swapi-app/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/swapi-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f544222a9601080676f7616bd8c5253e",
+    "url": "/swapi-app/index.html"
+  }
+];
